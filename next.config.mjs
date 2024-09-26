e8102f87692043c7554ea27ad3c541d19b382005
Emmanuel Quintana <eqb2021@gmail.com>
2024-09-26 18:10:55 -0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/home',
+                destination: '/a87s6d87as6bdas6d87as6d8asbd6a78s6d8a7s6d87as6d87as6d786bas8d6a8sd68a7sd',
                 permanent: true, // Marca como redirección permanente (301)
             },
         ];
